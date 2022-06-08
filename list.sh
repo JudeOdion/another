091e611dc99857f2b34a1e6fef55e43329c7ab3f
root <root@ip-172-31-39-78.ec2.internal>
2022-06-08 07:27:49 +0000

    branching to group

--- list.sh
+++ list.sh
@@ -1,9 +1,15 @@
 # This is a list of people in the 28th Landmark class
 # Two workers were added to the list today
 
+# I am adding these last three guys now. please note
+
 Omoruyi
 Osamu
 Lucky
 Fidelis
 Omorodion
 Iwinosa
+
+Imasuen
+Guobadia
+Orobosa
